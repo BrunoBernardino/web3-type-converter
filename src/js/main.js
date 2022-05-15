3c75d055f28aa1edfad268e1fa4d6ef9ac4c252e
teodor <31542280+tgbv@users.noreply.github.com>
2022-05-16 00:12:18 +0300

    added padding support for bytes32/hex

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,14 @@
   const input = document.getElementById('input');
   const toType = document.getElementById('to');
   const output = document.getElementById('output');
+  const applyPadding = document.getElementById('applyPadding');
+  const applyPaddingContainer = document.getElementById('applyPaddingContainer');
 
   const convert = (value) => {
     if (toType.value === 'bytes32') {
-      output.value = web3.toHex(value);
+      const v = web3.toHex(value);
+
+      output.value = applyPadding.checked ? web3.padRight(v, 66) : v;
     }
     if (toType.value === 'number') {
       output.value = web3.toDecimal(value);
@@ -22,5 +26,9 @@
 
   input.addEventListener('keyup', () => convert(input.value));
   input.addEventListener('blur', () => convert(input.value));
-  toType.addEventListener('change', () => convert(input.value));
+  toType.addEventListener('change', () => {
+    applyPaddingContainer.style.display = toType.value === 'bytes32' ? 'inline' : 'none';
+    convert(input.value);
+  });
+  applyPadding.addEventListener('change', () => convert(input.value));
 })();
