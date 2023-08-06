29026201ab41578bb5d4eb6a72390bb815cb2e00
Bruno Bernardino <me@brunobernardino.com>
2023-08-06 14:14:58 +0100

    Update Deno, remove stats, add better favicon

--- main.ts
+++ main.ts
@@ -1,19 +1,8 @@
-import { Application } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
-
-const baseUrl = 'https://web3-type-converter.onbrn.com';
+import { Application } from 'https://deno.land/x/oak@v12.6.0/mod.ts';
 
 const app = new Application();
 app.use(async (ctx) => {
   try {
-    const urlParts = ctx.request.url.pathname.split('/');
-    const file = urlParts.pop();
-    const fileExtension = file?.split('.').pop();
-    if ((!file && !fileExtension) || (file && fileExtension === 'html')) {
-      if (!ctx.request.url.hostname.startsWith('localhost')) {
-        recordPageView(ctx.request.url.pathname);
-      }
-    }
-
     await ctx.send({
       root: `${Deno.cwd()}/src`,
       index: 'index.html',
@@ -27,22 +16,3 @@ app.use(async (ctx) => {
 console.log('Listening on http://localhost:8000');
 
 await app.listen({ port: 8000 });
-
-async function recordPageView(pathname: string) {
-  try {
-    await fetch('https://stats.onbrn.com/api/event', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json; charset=utf-8',
-      },
-      body: JSON.stringify({
-        domain: baseUrl.replace('https://', ''),
-        name: 'pageview',
-        url: `${baseUrl}${pathname}`,
-      }),
-    });
-  } catch (error) {
-    console.log('Failed to log pageview');
-    console.error(error);
-  }
-}
